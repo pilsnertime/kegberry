32e9bcef1d19473439f82460311d167e88a22df4
pilsnertime <qwertyhunk69@gmail.com>
2017-10-01 02:59:02 -0700

    More fixes to solenoid

--- keg/solenoid.js
+++ keg/solenoid.js
@@ -6,11 +6,12 @@ class Solenoid {
 		this.safetyLimitMs = safetyLimitMs;
 		this.gpio = gpio;
 		this.gpio.setup(pin, gpio.DIR_OUT);
+		this.pin = pin;
 		this.safetyTimer = setTimeout(function(){},0);
 	}
 	
 	Open(callback) {
-		this.gpio.write(pin, true, (err) => {
+		this.gpio.write(this.pin, true, (err) => {
 			clearTimeout(this.safetyTimer);
 			this.safetyTimer = setTimeout(() => {this.safetyClose()}, this.safetyLimitMs);
 			callback(err);
@@ -18,7 +19,7 @@ class Solenoid {
 	}
 
 	Close(callback) {
-		this.gpio.write(pin, false, (err) => {
+		this.gpio.write(this.pin, false, (err) => {
 			clearTimeout(this.safetyTimer);
 			// we want the app to crash if the close fails
 			if (err) throw err;
@@ -27,7 +28,7 @@ class Solenoid {
 	}
 
 	safetyClose() {
-		this.gpio.write(pin, false, (err) => {
+		this.gpio.write(this.pin, false, (err) => {
 			console.log("Closed the solenoid forcefully because the safety timer went off to prevent the solenoid from burning.");
 			if (err) throw err;
 		});
@@ -48,7 +49,7 @@ class MockSolenoid {
 
 function createSolenoid(params) {
 	
-	var pin = params.pin ? params.pin : 40;
+	var pin = params.pin ? params.pin : 38;
 	var safetyLimitMs = params.safetyLimitMs ? params.safetyLimitMs : 30000
 	var gpio = require('rpi-gpio');
 
