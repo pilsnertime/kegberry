9a71e89db0df49d4d443faa6821e57f22127022a
Pilsnertime <ilshat.ahatov@gmail.com>
2017-10-01 01:16:20 -0700

    Move functionality to kegEngine (#12)
    * Move functionality to kegEngine

--- app.js
+++ app.js
@@ -29,34 +29,12 @@ weatherProcess.on('error', (err) => {
   console.log("Couldn't spawn temperature polling. Make sure python is installed.")
 });
 
-var MOCK_POURS = false;
-
 var flowmeter = require('./keg/flowmeter')({
 	pin: 40,
 	tickCalibration: 0.00089711713,
 	timeBetweenPours: 3000, 
-	notificationMl: 10,
-  mock: MOCK_POURS
+	notificationMl: 10
 });
 
-var messageService = require('./keg/messageService.js')(users, pours);
-
-wss.on('connection', function connection(ws) {
-  console.log("Web socket client connected.");
-
-  ws.on('message', function incoming(message) {
-    console.log('Recieved a message from the client: %s', message);
-    messageService.process(message, ws);
-
-    if (MOCK_POURS)
-    {
-      messageService.mockPour(message, flowmeter);
-    }
-
-  });
-
-  messageService.weatherUpdate(weatherProcess.stdout, ws);
-
-  messageService.pourUpdate(flowmeter, ws);
-
-});
+var messageService = require('./keg/messageService.js')(wss, weatherProcess, flowmeter, users, pours);
+messageService.Start();
--- keg/messageService.js
+++ keg/messageService.js
@@ -1,106 +1,18 @@
-class ClientMessage 
+var KegEngine = require('./kegEngine');
+const Configuration = require('./configuration');
+const {BadRequestResponseMessage} = require('./definitions');
+
+class MessageChannel
 {
-    constructor(msg)
+    constructor(ws)
     {
-        this.messageName = msg.name;
-        this.data = msg.data;
-    }
-}
+        this.ws = ws;
+    }    
 
-class ResponseMessage
-{
-    constructor(name, err, data)
-    {
-        this.messageName = name;
-        this.error = err;
-        this.data = data;
-    }
-}
-
-class WeatherNotificationMessage extends ResponseMessage
-{
-    constructor(err, weatherData)
-    {
-        super("weather", err, weatherData);
-    }
-}
-
-class PourNotificationMessage extends ResponseMessage
-{
-    constructor(currentUser, litersPoured, totalPour, finishedPour)
-    {
-        super("pourUpdate", null, {
-            currentUser: currentUser,
-            incrementalPour: litersPoured,
-            totalPour: totalPour,
-            isFinished: finishedPour
-        });
-    }
-}
-
-class AddUserMessage extends ClientMessage
-{
-    constructor(msg)
-    {
-        super(msg);
-        if (msg.data) {
-            this.userName = msg.data.name;
-        }
-    }
-}
-
-class AddUserResponseMessage extends ResponseMessage
-{
-    constructor(err, user)
-    {
-        super("addUserResponse", err, !user?undefined:{user: user});
-    }
-}
-
-class GetUsersResponseMessage extends ResponseMessage
-{
-    constructor(err, users)
-    {
-        super("getUsersResponse", err, !users?undefined:{users: users});
-    }
-}
-
-class SelectUserResponseMessage extends ResponseMessage
-{
-    constructor(err, user)
-    {
-        super("selectUserResponse", err, !user?undefined:{user: user});
-    }
-}
-
-class BadRequestResponseMessage extends ResponseMessage
-{
-    constructor(message)
-    {
-        super("failedResponse", message)
-    }
-}
-
-function MessageService(users, pours)
-{
-    this.users = users;
-    this.pours = pours;
-
-    this.initialize = () => {
-        this.users.getDefaultUser((err, user) => {
-            if (!err) {
-                this.currentUser = user;
-            } else {
-                console.log("Error while getting the default user: " + err);
-            }
-        });
-
-        this.currentPourTotal = 0;
-    }
-
-    this.sendMessage = (responseMsg, ws) => {
+    // Send message on the websocket channel. Returns true if the connection closed.
+    SendMessage(responseMsg) {
         try {
-            ws.send(JSON.stringify(responseMsg));
+            this.ws.send(JSON.stringify(responseMsg));
         } catch (e) {
             if (e.message == "not opened") {
                 console.log("Socket connection closed before message could be sent..");
@@ -111,8 +23,45 @@ function MessageService(users, pours)
         }
         return false;
     };
+}
 
-    this.process = (msg, ws) => {
+class MessageService
+{
+    constructor(wss, weatherProcess, flowmeter, users, pours)
+    {
+        this.wss = wss;
+        this.weatherProcess = weatherProcess;
+        this.flowmeter = flowmeter;
+        this.users = users;
+        this.pours = pours;
+    }
+
+    Start() {
+        if (!this.kegEngine) {
+            this.wss.on('connection', (ws) => {
+                console.log("Web socket client connected.");
+
+                ws.on('message', (message) => {
+                    console.log('Recieved a message from the client: %s', message);
+                    this.process(message, ws);
+                    if (Configuration.MOCK_POURS)
+                    {
+                        this.mockPour(message);
+                    }
+
+                });
+                
+                this.messageChannel = new MessageChannel(ws);
+                this.kegEngine = new KegEngine(this.messageChannel, this.users, this.pours);
+
+                this.kegEngine.Initialize();
+                this.kegEngine.HandleWeather(this.weatherProcess.stdout);
+                this.kegEngine.HandlePours(this.flowmeter);
+            });
+        }
+    }
+
+    process(msg, ws) {
 
         var parsedMsg;
         try 
@@ -121,41 +70,29 @@ function MessageService(users, pours)
         } 
         catch (e)
         {
-            responseMsg = new BadRequestResponseMessage("Failed to parse message: " + e.message);
-            this.sendMessage(responseMsg, ws);
+            var responseMsg = new BadRequestResponseMessage("Failed to parse message: " + e.message);
+            this.messageChannel.SendMessage(responseMsg, ws);
             return;
         } 
 
         if (parsedMsg && !parsedMsg.messageName){
-            responseMsg = new BadRequestResponseMessage("All messages must contain a 'messageName' property");
-            this.sendMessage(responseMsg, ws);
+            var responseMsg = new BadRequestResponseMessage("All messages must contain a 'messageName' property");
+            this.messageChannel.SendMessage(responseMsg, ws);
             return;
         }
 
         switch(parsedMsg.messageName)
         {
             case "addUser":
-                this.users.addUser(new AddUserMessage(parsedMsg), (err, guid) => {
-                    responseMsg = new AddUserResponseMessage(err, guid);
-                    this.sendMessage(responseMsg, ws);
-                });
+                this.kegEngine.AddUser(parsedMsg);
                 break;
 
             case "getUsers":
-                this.users.getUsers((err, users) => {
-                    responseMsg = new GetUsersResponseMessage(err, users);
-                    this.sendMessage(responseMsg, ws);
-                });
+                this.kegEngine.GetUsers(parsedMsg);
                 break;
 
             case "selectUser":
-                this.users.getUser(parsedMsg.data.id, (err, user) => {
-                    if (!err) {
-                        this.currentUser = user;
-                    }
-                    responseMsg = new SelectUserResponseMessage(err, this.currentUser);
-                    this.sendMessage(responseMsg, ws);
-                });
+                this.kegEngine.SelectUser(parsedMsg);
                 break;
 
             case "fakePour":
@@ -163,12 +100,12 @@ function MessageService(users, pours)
                 break;
 
             default:
-                responseMsg = new BadRequestResponseMessage("Unknown operation '" + parsedMsg.name + "'");
-                this.sendMessage(responseMsg, ws);
+                var responseMsg = new BadRequestResponseMessage("Unknown operation '" + parsedMsg.messageName + "'");
+                this.messageChannel.SendMessage(responseMsg, ws);
         }
     };
 
-    this.mockPour = (msg, flowmeter) => {
+    mockPour(msg) {
         var parsedMsg;
         try 
         {
@@ -176,74 +113,22 @@ function MessageService(users, pours)
         } 
         catch (e)
         {
-            responseMsg = new BadRequestResponseMessage("Failed to parse message: " + e.message);
-            this.sendMessage(responseMsg, ws);
+            var responseMsg = new BadRequestResponseMessage("Failed to parse message: " + e.message);
+            this.messageChannel.SendMessage(responseMsg, ws);
             return;
         } 
         switch(parsedMsg.messageName)
         {
             case "fakePour":
-                flowmeter.emit("fakePour");
+                this.kegEngine.FakePour(this.flowmeter);
                 break;
         }
     };
-
-    this.weatherUpdate = (channel, ws) => {
-
-        var weatherCallback = (data) => {
-            if(data){
-                var outData = JSON.parse(data);
-                var notificationMsg = new WeatherNotificationMessage(outData.error, outData.data);
-                if (this.sendMessage(notificationMsg, ws)) {
-                    channel.removeListener('data', weatherCallback);
-                }
-            }    
-        };
-        
-        channel.on('data', weatherCallback);
-    };
-
-    this.pourUpdate = (flowmeter, ws) => {
-
-        var pourCallback = (litersPoured) => {
-            if(litersPoured){
-                this.currentPourTotal += litersPoured;
-                var notificationMsg = new PourNotificationMessage(this.currentUser, litersPoured, this.currentPourTotal, false);
-                if(this.sendMessage(notificationMsg, ws)) {
-                    flowmeter.removeListener('pourUpdate', pourCallback);
-                }
-            }
-        };
-
-        var finishedPourCallback = (litersPoured) => {
-            if(litersPoured){
-                this.currentPourTotal += litersPoured;
-                this.pours.addPour({userId: this.currentUser.id, beerId: "shocktop", amount: this.currentPourTotal}, (err, res) => {
-                    if (err || !res) {
-                        console.log("Failed to preserve pour. Error: " + err);
-                    }
-                });
-
-                var notificationMsg = new PourNotificationMessage(this.currentUser, litersPoured, this.currentPourTotal, true);
-
-                this.currentPourTotal = 0;
-                if(this.sendMessage(notificationMsg, ws)) {
-                    flowmeter.removeListener('finishedPour', finishedPourCallback);
-                }
-            }
-        };
-
-        flowmeter.on('pourUpdate', pourCallback);
-
-        flowmeter.on('finishedPour', finishedPourCallback);
-    };
 }
 
-function ExposeMessageService(users, pours)
+function ExposeMessageService(wss, weatherProcess, flowmeter, users, pours)
 {
-    var messageService = new MessageService(users, pours);
-    messageService.initialize();
-    return messageService;
+    return new MessageService(wss, weatherProcess, flowmeter, users, pours);
 }
 
 module.exports = ExposeMessageService;
