5abe1db574b8c36d47b6dd49930b6c3164336f66
pilsnertime <qwertyhunk69@gmail.com>
2017-10-22 19:48:16 -0700

    Set mockpours to false

--- keg/configuration.js
+++ keg/configuration.js
@@ -1,5 +1,5 @@
 class Configuration {
-    get MOCK_POURS () { return true };
+    get MOCK_POURS () { return false };
     get USER_TIMEOUT() { return 15000 };
     get CALIBRATION_ML() { return 237 };
 }
